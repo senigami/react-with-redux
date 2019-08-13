accd39767114055ca477678cb00837783b3bf146
senigami <sd@stevenandnicole.com>
2019-08-13 16:31:28 -0400

    language

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends Component {
+	static contextType = LanguageContext;
+
+	renderButton(color) {
+		return (
+			<button className={`ui button ${color}`}>
+				<LanguageContext.Consumer>
+					{({ language }) => (language === 'english' ? 'Submit' : 'Voorleggen')}
+				</LanguageContext.Consumer>
+			</button>
+		);
+	}
+
+	render() {
+		return <ColorContext.Consumer>{color => this.renderButton(color)}</ColorContext.Consumer>;
+	}
+}
+
+export default Button;
